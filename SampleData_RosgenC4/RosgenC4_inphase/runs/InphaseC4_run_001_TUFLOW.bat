96aa92fb9c42f3bc6015d1a66d26e2a3fa0a3c0a
anzylee <anzylee7@gmail.com>
2020-10-18 21:49:47 -0600

    update_result

--- SampleData_RosgenC4/RosgenC4_inphase/runs/InphaseC4_run_001_TUFLOW.bat
+++ SampleData_RosgenC4/RosgenC4_inphase/runs/InphaseC4_run_001_TUFLOW.bat
@@ -1,5 +1,5 @@
 Set TF_Exe="C:\Program Files\Tuflow_w64\TUFLOW_iSP_w64.exe"
 Set RUN=start "TUFLOW" %TF_Exe%
 
-%RUN% -b "C:\Users\waterlab\Documents\GitHub\RiverArchitect-tutorial\SampleData_RosgenC4\RosgenC4_inphase\runs\InphaseC4_001.tcf"
+%RUN% -b "F:\RiverArchitect-tutorial\SampleData_RosgenC4\RosgenC4_Inphase\runs\InphaseC4_001.tcf"
 pause
